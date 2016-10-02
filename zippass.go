9813f243cc0534edd919e1507c2ce30282d5c397
Yuki Obara <obaray@toilab.jp>
2016-10-02 21:05:33 +0900

    add files

---
+++ zippass.go
@@ -0,0 +1,14 @@
+package zippass
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func init() {
+	http.HandleFunc("/", handler)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello, toilab!!!!")	
+}
