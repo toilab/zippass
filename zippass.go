9aabbe0c47e1b84644d4bbfe86f7994cbcbf7c0a
Yuki Obara <obaray@toilab.jp>
2016-10-03 19:59:54 +0900

    zippass.go

--- zippass.go
+++ zippass.go
@@ -1,14 +1,34 @@
 package zippass
 
 import (
-	"fmt"
+	_ "fmt"
 	"net/http"
+	_ "html/template"
+	
+	"github.com/gin-gonic/gin"
 )
 
+// initialize
 func init() {
-	http.HandleFunc("/", handler)
+	// http.HandleFunc("/", handler)
+	http.Handle("/", GetMainEngine())
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello, toilab!!!!")	
+func GetMainEngine() *gin.Engine {
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*")
+	router.GET("/test/:id", TestGet)
+	router.POST("/zippass/", ZipPass)
+	return router
+}
+
+func TestGet(g *gin.Context) {
+	ids := g.Param("id")
+	g.HTML(http.StatusOK, "t.html", gin.H{
+		"KeyIDs": ids,
+	})
+}
+
+func ZipPass(g *gin.Context) {
+	g.String(200, "Request is received.")
 }
