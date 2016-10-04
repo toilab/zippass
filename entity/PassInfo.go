4ca9cb344eeb7bd2fcb8f7b381d514a8521a523f
yuki <y-obara@in-forest.jp>
2016-10-04 23:26:32 +0900

    POSTできるようになった

---
+++ entity/PassInfo.go
@@ -0,0 +1,15 @@
+package entity
+
+import (
+
+)
+
+// pass情報
+type PassInfo struct {
+	PassTypeID			string	`json:"passtype_id"`	// パス型識別子
+	SerialNumber		string	`json:"serial_num"`		// シリアルNo
+	TeamID				string	`json:"team_id"`		// チーム識別子
+	FormatVersion		int		`json:"format_ver"`		// ファイルフォーマットVer 現在1固定
+	Description			string	`json:"description"`	// このpassの簡潔な説明
+	OrganizationName	string	`json:"org_name"`		// 組織名
+}
--- zippass.go
+++ zippass.go
@@ -1,34 +1,83 @@
 package zippass
 
 import (
-	_ "fmt"
+	"fmt"
 	"net/http"
 	_ "html/template"
+	"log"
 	
 	"github.com/gin-gonic/gin"
+	// "github.com/comail/colog"
+	
+	"entity"
+	
+	
 )
 
 // initialize
 func init() {
+	// set logger
+	
+	// SetLogger()
+	
 	// http.HandleFunc("/", handler)
 	http.Handle("/", GetMainEngine())
+	
+	// colog.Register()
+	// log.Printf("info: colog registered.")
 }
 
+/*
+func SetLogger() {
+	colog.SetDefaultLevel(colog.LDebug)
+	colog.SetMinLevel(colog.LTrace)
+	colog.SetFormatter(&colog.StdFormatter{
+		Colors:	true,
+		Flag:	log.Ldate | log.Ltime | log.Lshortfile,
+	})
+	colog.Register()
+	
+	log.Printf("info: colog registered.")
+}
+*/
 func GetMainEngine() *gin.Engine {
+	log.Printf("debug: start %s", "GetMainEngine")
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*")
+	router.GET("/", RootGet)
 	router.GET("/test/:id", TestGet)
 	router.POST("/zippass/", ZipPass)
+	router.POST("/test/", TestPost)
+	log.Printf("debug: end %s", "GetMainEngine")
 	return router
 }
 
+func RootGet(g *gin.Context) {
+	log.Printf("debug: start %s", "RootGet")
+	g.HTML(http.StatusOK, "index.html", nil)
+}
+
 func TestGet(g *gin.Context) {
+	log.Printf("debug: start %s", "TestGet")
 	ids := g.Param("id")
 	g.HTML(http.StatusOK, "t.html", gin.H{
 		"KeyIDs": ids,
 	})
 }
 
+func TestPost(g *gin.Context) {
+	log.Printf("debug: start %s", "TestPost")
+	var passInfo entity.PassInfo
+	j := g.BindJSON(&passInfo)
+	log.Printf(fmt.Sprintf("PassTypeID is %s.", passInfo.PassTypeID))
+	if j != nil {
+		
+		g.String(200, "kitakita")
+	}
+}
+
 func ZipPass(g *gin.Context) {
+	log.Printf("debug: start %s", "ZipPass")
+	
 	g.String(200, "Request is received.")
 }
